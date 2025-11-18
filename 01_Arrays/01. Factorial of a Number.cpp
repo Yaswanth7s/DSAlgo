6ab41d9b6daad610ab42cad779a090a97e242f0f
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2025-11-18 22:09:57 +0530

    Implement factorial calculation in C++
    Added a C++ program to calculate the factorial of a number using recursion.

--- 01_Arrays/01. Factorial of a Number.cpp
+++ 01_Arrays/01. Factorial of a Number.cpp
@@ -1,1 +1,21 @@
+//Finding the factorial of a number
+//Time complexity: O(n)
+//Space complexity: O(n)
+#include<iostream>
+using namespace std;
+int factorial(int n){
+	if(n<=1){
+		return 1;
+	}
+	else{
+		return n*factorial(n-1);
+	}
+}
+int main(){
+	int n;
+	cout<<"Enter the Number: "<<endl;
+	cin>>n;
+	cout<<"Factorial of "<<n<<" is: "<<factorial(n)<<endl;
+}
+
 
