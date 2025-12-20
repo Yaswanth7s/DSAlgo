d97187d7cf08e717252a14c9b2019c24c9e2a8d6
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2025-12-20 07:54:41 +0530

    Add min in a rotated sorted array solution
    Implemented two methods to find the minimum element in a rotated sorted array. The first method uses brute force with O(n) time complexity, and the second method uses binary search with O(log n) time complexity.

---
+++ 02.Binary Search/08.Min in a Rotated Sorted Array(Unique).cpp
@@ -0,0 +1,62 @@
+                            //Findig minimum in a rotated sorted array (Unique elements)
+//Method-1: Brute Force
+//Time complexity: O(n)
+//Space complexity: O(1)
+#include<bits/stdc++.h>
+using namespace std;
+int mininrotatedsortedarray(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            return arr[i+1];
+        }
+    }
+    return arr[0];
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements into the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int result=mininrotatedsortedarray(arr,n);
+        cout<<"Min element is: "<<result;
+}
+
+
+//Method-2: Optimal Solution
+//Time complexity: O(log n)
+//Space complexity: O(1)
+#include<bits/stdc++.h>
+using namespace std;
+int mininrotatedsortedarray(int arr[],int n){
+    int low=0;
+    int high=n-1;
+    int ans=INT_MAX;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[low]<=arr[mid]){
+            ans=min(ans,arr[low]);
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+            ans=min(ans,arr[mid]);
+        }
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements into the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int result=mininrotatedsortedarray(arr,n);
+    cout<<"Minumum from the rotated sorted array is: "<<result;
+}
