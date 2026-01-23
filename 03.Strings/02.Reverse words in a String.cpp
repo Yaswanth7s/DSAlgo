f978d3fd06b1920f456d8a839eeba4c6de8a9427
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-01-23 22:04:25 +0530

    Add function to reverse words in a string
    Implement a function to reverse the words in a string.

---
+++ 03.Strings/02.Reverse words in a String.cpp
@@ -0,0 +1,30 @@
+                                             //Reverse the words in a String
+//Method-1: Better/Optimal Solution
+//Time complexity: O(n)
+//Space complexity: O(n)
+#include<bits/stdc++.h>
+using namespace std;
+string Reversewordsinastring(string s){
+    int n=s.length();
+    string ans="";
+    reverse(s.begin(),s.end());
+    for(int i=0;i<n;i++){
+        string word="";
+        while(i<n && s[i]!=' '){
+            word+=s[i];
+            i++;
+        }
+        reverse(word.begin(),word.end());
+        if(word.length()>0){
+            ans+=" "+word;
+        }
+    }
+    return ans.substr(1);
+}
+int main(){
+    string s;
+    cout<<"Enter the string: ";
+    getline(cin,s);
+    string result=Reversewordsinastring(s);
+    cout<<"After reversing words in a string: "+result;
+}
