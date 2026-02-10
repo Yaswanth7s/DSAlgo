849fbb8c6f2a532874553827bb98b6cae67ccce5
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-02-10 22:46:42 +0530

    Add conversion function from array to linked list

---
+++ 04.Linked List/02.Conversion Of Array to Linked List.cpp
@@ -0,0 +1,36 @@
+                                //Conversion Of Array to Linked List
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+    
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+Node* convertArr2LL(vector<int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> arr={3,8,2,9};
+    Node* head= convertArr2LL(arr);
+    cout<<head->data;
+}
